afc902149cc0dfb95e0b96981d7acd598643814b
jaey8den <89349331+jaey8den@users.noreply.github.com>
2025-07-17 16:45:17 +0800

    v0.1.2 -> Commenting run command as it is deployed

--- main.py
+++ main.py
@@ -89,5 +89,5 @@ async def process_instructions(instructions: Instructions):
 async def get_diagram():
     return FileResponse(f"diagrams/output.jpg")
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
